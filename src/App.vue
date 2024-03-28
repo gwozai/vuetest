04e804873c76b2248a958899cccb04b4bec8a1c3
gwozai <63190594+gwozai@users.noreply.github.com>
2024-03-28 19:10:28 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import { ref } from 'vue';
-import Listmedia from './components/listmedia.vue'
-import Mediaaudio from './components/mediaaudio.vue'
-import Suijiyiju from './components/suijiyiju.vue';
-import Videotest from './components/videotest.vue';
-// import Markdowntest from './components/markdowntest.vue';
-
-const showComponent = ref(false);
-
-function toggleComponent() {
-  showComponent.value = !showComponent.value;
-}
-</script>
-
 <template>
   <main>
     <p>vue 主页</p>
 
-    <Mediaaudio />
-    <Videotest />
-    <Suijiyiju />
-    <!-- <Markdowntest /> -->
-    <button @click="toggleComponent">测试部分</button>
-    <Listmedia v-if="showComponent" />
+    <button @click="isDialogVisible = true">选择要显示的组件</button>
+
+    <el-dialog title="请选择要显示的组件" :visible.sync="isDialogVisible">
+      <button v-for="component in components" @click="selectedComponent = component">{{ `显示 ${component}` }}</button>
+    </el-dialog>
+
+    <component :is="selectedComponent"></component>
   </main>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+  import { ElDialog } from 'element-plus';
+  import Listmedia from './components/listmedia.vue'
+  import Mediaaudio from './components/mediaaudio.vue'
+  import Suijiyiju from './components/suijiyiju.vue';
+  import Videotest from './components/videotest.vue';
+
+  const components = ['Mediaaudio', 'Videotest', 'Suijiyiju', 'Listmedia'];
+  const selectedComponent = ref(components[0]);
+  const isDialogVisible = ref(false);
+</script>
 
 <style scoped>
 </style>
--- src/components/mediaaudio.vue
+++ src/components/mediaaudio.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="container">
-    <button class="btn" @click="showPopup = !showPopup">
-      {{ showPopup ? "隐藏" : "显示" }}播放内容
-    </button>
-    <div v-if="showPopup" class="popup">
-      正在播放：{{ mediaSource }}
-    </div>
+
+
     <video class="player" controls autoplay :name="mediaName" crossorigin="anonymous">
       <source :src="mediaSource" :type="mediaType">
     </video>
@@ -27,36 +23,5 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  text-align: center;
-  background: url('./../assets/202403280345955.jpg') no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  height: 100vh;
-}
-.btn {
-  display: inline-block;
-  margin: 20px 0;
-  padding: 10px 20px;
-  background-color: transparent;
-  color: white;
-  border: 2px solid white;
-  border-radius: 5px;
-  cursor: pointer;
-}
-.popup {
-  margin: 20px 0;
-  padding: 10px;
-  color: white;
-  border: 1px solid white;
-  border-radius: 5px;
-  background-color: rgba(255, 255, 255, 0.6);
-}
-.player {
-  max-width: 80%;
-  height: auto;
-  margin: 0 auto;
-}
+
 </style>
