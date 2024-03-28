04c7bbb8043a595e7c0647183473bebc983def7a
gwozai <63190594+gwozai@users.noreply.github.com>
2024-03-28 13:13:28 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script setup>
 import Listmedia from './components/listmedia.vue'
 import Mediaaudio from './components/mediaaudio.vue'
-import Localmedia from './components/localmedia.vue'
+
+
 </script>
 
 <template>
-
-
   <main>
     <p>vue 主页</p>
-    <Localmedia />
-    <Listmedia />
+
     <Mediaaudio />
+
+    <p>测试部分</p>
+    <Listmedia />
+
+
+
   </main>
 </template>
 
