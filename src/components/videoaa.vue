48e1eea6dc98174547b4f529408ff90a068a828b
gwozai <63190594+gwozai@users.noreply.github.com>
2024-03-29 00:20:53 +0800

    22

--- src/components/videoaa.vue
+++ src/components/videoaa.vue
@@ -1,31 +1,38 @@
 <template>
-    <div>
-      <video-player :options="videoOptions" />
-    </div>
-  </template>
-  
-  <script>
-  import VideoPlayer from '@/components/VideoPlayer.vue';
-  
-  export default {
-    name: 'VideoExample',
-    components: {
-      VideoPlayer
-    },
-    data() {
-      return {
-        videoOptions: {
-          autoplay: true,
-          controls: true,
-          sources: [
-            {
-              src:
-                'src/assets/1.mp4',
-                type: 'video/mp4'
-            }
-          ]
-        }
-      };
+  <video-player
+      class="video-player vjs-custom-skin"
+      ref="videoPlayer"
+      :playsinline="true"
+      :options="playerOptions"/>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    playerOptions: {
+      playbackRates: [0.7, 1.0, 1.5, 2.0],
+      autoplay: false,
+      muted: false,
+      loop: false,
+      preload: 'auto',
+      language: 'zh-CN',
+      aspectRatio: '16:9',
+      fluid: true,
+      sources: [{
+        type: "video/mp4",
+        src: "src/assets/1.mp4" //你的视频文件路径
+      }],
+      poster: "https://p1.music.126.net/5zs7IvmLv7KahY3BFzUmrg==/109951163635241613.jpg?param=600y500",
+      notSupportedMessage: '此视频暂无法播放，请稍后再试',
+      controlBar: {
+        timeDivider: true,
+        durationDisplay: true,
+        remainingTimeDisplay: false,
+        fullscreenToggle: true
+      }
     }
-  };
-  </script>
+  }
+}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 // import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import videojs and related css
+import VueVideoPlayer from 'vue-video-player'
+import 'video.js/dist/video-js.css'
+import 'vue-video-player/src/custom-theme.css'
+
+// Create app
+const app = createApp(App)
+
+// Use video player
+app.use(VueVideoPlayer)
+
+// Mount app
+app.mount('#app')
