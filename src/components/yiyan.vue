fc4242f590d98fd0e234426bfd0959b0032105e5
gwozai <63190594+gwozai@users.noreply.github.com>
2024-03-30 18:39:49 +0800

    1

--- src/components/yiyan.vue
+++ src/components/yiyan.vue
@@ -49,7 +49,7 @@
         }
         this.loading = true;
         try {
-          const response = await axios.get("/api", {
+          const response = await axios.get("/api/", {
             params: { category: this.selectedCategory },
             headers: {
                 'Accept': 'application/json'
