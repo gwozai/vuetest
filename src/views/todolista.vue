58fdf205ee7e1eeeb403cb5d235935279132ffd7
gwozai <63190594+gwozai@users.noreply.github.com>
2024-04-02 11:22:45 +0800

    131

--- src/views/todolista.vue
+++ src/views/todolista.vue
@@ -1,35 +1,44 @@
-// ExampleComponent.vue
-
 <template>
-  <div>
-    <button @click="createTask">Create Task</button>
-    <button @click="getAllTasks">Get All Tasks</button>
-    <button @click="getTaskById(2)">Get Task by ID</button>
-    <button @click="updateTask(3)">Update Task</button>
-    <button @click="deleteTask(1)">Delete Task</button>
-  </div>
-</template>
-
-<script>
-import apiUtils from '@/jscom/apiUtils.js';
-
-export default {
-  methods: {
-    async createTask() {
-      await apiUtils.testCreateTask("test task","test description","1");
+    <div>
+      <input v-model="newTaskTitle" type="text" placeholder="Task Title">
+      <input v-model="newTaskDescription" type="text" placeholder="Task Description">
+      <input v-model="taskId" type="text" placeholder="Task ID">
+      <button @click="createTask">Create Task</button>
+      <button @click="getAllTasks">Get All Tasks</button>
+      <button @click="getTaskById(taskId)">Get Task by ID</button>
+      <button @click="updateTask(taskId)">Update Task</button>
+      <button @click="deleteTask(taskId)">Delete Task</button>
+    </div>
+  </template>
+  
+  <script>
+  import apiUtils from '@/jscom/apiUtils.js';
+  
+  export default {
+    data() {
+      return {
+        newTaskTitle: '',
+        newTaskDescription: '',
+        taskId: ''
+      };
     },
-    async getAllTasks() {
-      await apiUtils.testGetAllTasks();
-    },
-    async getTaskById(taskId) {
-      await apiUtils.testGetTaskById(taskId);
-    },
-    async updateTask(taskId) {
-      await apiUtils.testUpdateTask(taskId);
-    },
-    async deleteTask(taskId) {
-      await apiUtils.testDeleteTask(taskId);
+    methods: {
+      async createTask() {
+        await apiUtils.testCreateTask(this.newTaskTitle, this.newTaskDescription);
+      },
+      async getAllTasks() {
+        await apiUtils.testGetAllTasks();
+      },
+      async getTaskById(taskId) {
+        await apiUtils.testGetTaskById(taskId);
+      },
+      async updateTask(taskId) {
+        await apiUtils.testUpdateTask(taskId);
+      },
+      async deleteTask(taskId) {
+        await apiUtils.testDeleteTask(taskId);
+      }
     }
-  }
-};
-</script>
+  };
+  </script>
+  
